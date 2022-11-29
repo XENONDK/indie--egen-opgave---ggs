9af5c969111b7927ab6a5523384dcabf45d829ae
XENONDK <dch3005@gmail.com>
2022-11-29 18:21:06 +0100

    fixed bugs

--- karakter.js
+++ karakter.js
@@ -15,9 +15,12 @@ const title = document.querySelector("#title")
 const desc = document.querySelector("#desc")
 
 let characterInfo = {
-    "Sol-Badguy":{Name: "Sol Badguy", description: "Sol is a top tier character that is perfect for beginners. He rarely needs to do no more than spam 5S to win and even if that fails he have volcanic viper. All in all sol is a glue eating poster boy that is perfect for beginners.", difficulty: 5, Title: "The Flame of Corruption"},
+    "Sol-Badguy":{Name: "Sol Badguy", description: "Sol is a top tier character that is perfect for beginners. He rarely needs to do no more than spam 5S to win and even if that fails he have volcanic viper. All in all sol is a glue eating poster boy that is perfect for beginners.", difficulty: 5, cTitle: "The Flame of Corruption"},
     
-    "Ramlethal-Valentine":{Name: "Ramlethal Valentine", description: "this MF wack yo", difficulty: 3},
+    "Sin-Kiske": {Name: "Sin Kiske", description: "sin is a child stuck in a mans body. he believes children come from their parents eyes and is generally a moron. he likes to wave his flag around and he uses it as his primary weapon of choice. Sin is a great charater that have great potential to become great", difficulty: 3, cTitle: "Wild Boy"},
+
+    "Ramlethal-Valentine":{Name: "Ramlethal Valentine", description: "this MF wack yo. she got 2 really big swords that she yeets around the battlefield. she also got this weird kind of floating thingy majingy that speaks to her. she is incredibly annoying to play against so, dont play her, thank you.", difficulty: 4, cTitle: "Sword Lady"},
+    
     
 }
 
@@ -25,6 +28,6 @@ let character = characterInfo[urlObject.searchParams.get("hero")]
 
 characterName.innerHTML = character.Name
 difficultyLevel.innerHTML = character.difficulty
-title.innerHTML = character.Title
+title.innerHTML = character.cTitle
 desc.innerHTML = character.description
 
