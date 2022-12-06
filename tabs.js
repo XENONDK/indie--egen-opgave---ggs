c87d021149104eb5f7b3057c42665746a355310c
XENONDK <dch3005@gmail.com>
2022-12-06 23:06:17 +0100

    update 06-12-2022 23:06

---
+++ tabs.js
@@ -0,0 +1,26 @@
+
+function openTab(evt, tabName) {
+
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+
+
+}
+
+//  Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
