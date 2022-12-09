6a870e1f8d534e3613a3572dab26ec8ee91095bd
XENONDK <dch3005@gmail.com>
2022-12-09 14:01:25 +0100

    final commit

---
+++ validate.js
@@ -0,0 +1,9 @@
+
+const form = document.querySelector(".form")
+const formName = document.querySelector("#navn")
+const email = document.querySelector("#email")
+const msg = document.querySelector("#besked")
+
+form.addEventListener("submit", function(){
+    alert("sorry this is a work in progress")
+})
